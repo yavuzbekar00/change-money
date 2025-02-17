1ace949ec755cbd10304c9db4a023e00ef4c768f
yavuzbekar00 <yavuz.00.bekar@gmail.com>
2025-02-17 14:12:11 +0300

    add api

--- app/page.js
+++ app/page.js
@@ -1,11 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        fetch(
+          "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_W26NjyvBmRgD4D8brUPTc3n3OeZxte5nvKE8Gewe"
+        )
+          .then((response) => response.json())
+          .then((myData) => setDatas(myData.data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(datas);
   return (
     <div>
-      <div>deneem</div>
-      <div>deneme2</div>
+      {datas && Object.keys(datas).length > 0 ? (
+        Object.keys(datas).map((key) => (
+          <div key={key}>
+            <h1>{key}</h1>
+            <div>{datas[key]}</div>
+          </div>
+        ))
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
